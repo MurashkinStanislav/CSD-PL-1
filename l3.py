a4db2764f97616c77ee63f8721a323e610adad23
MurashkinStanislav <medvedsvodkoi@gmail.com>
2017-01-23 14:20:46 +0700

    Update l3.py

--- l3.py
+++ l3.py
@@ -1,48 +1,48 @@
 package main
 
 import (
-"fmt"
-"math/rand"
-"time"
+  "fmt"
+  "math/rand"
+  "time"
 )
 
 type Token struct {
-data string
-recipient int //Номер получателя
+  data string
+  recipient int //Номер получателя
 }
 
 var ch1 chan Token = make(chan Token)
 var deep int = 0
 
 func set(t Token) { //Функция передачи в канал
-ch1 <- t
+  ch1 <- t
 
 }
 
 func get() { // Функция взятия из канала
-curToken := <-ch1
-go set(curToken)
+  curToken := <-ch1
+  go set(curToken)
 
 if deep != curToken.recipient {
-deep++
-go get()
+  deep++
+  go get()
 } else {
-fmt.Println("Номер текущего потока: ", deep)
-fmt.Println("Токен: ", <-ch1)
+  fmt.Println("Номер текущего потока: ", deep)
+  fmt.Println("Токен: ", <-ch1)
 }
 
 }
 func main() {
-rand.Seed(time.Now().UnixNano())
-N := rand.Intn(100)
-fmt.Println("Номер потока адресата: ", N)
+  rand.Seed(time.Now().UnixNano())
+  N := rand.Intn(100)
+  fmt.Println("Номер потока адресата: ", N)
 
-var startToken Token
-startToken.recipient = N
-startToken.data = "smth data"
+  var startToken Token
+  startToken.recipient = N
+  startToken.data = "smth data"
 
-go set(startToken)
-go get()
-time.Sleep(100000000)
+  go set(startToken)
+  go get()
+  time.Sleep(100000000)
 
 }
